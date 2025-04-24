64926105926d8e85394d6492e66d110844ebacb7
Kellytowwife <kellyelitetowing@gmail.com>
2025-04-24 16:39:18 -0700

    Fix JSX parsing error by switching to createElement

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import SeedsOfHopeApp from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <SeedsOfHopeApp />
-  </React.StrictMode>
+  React.createElement(React.StrictMode, null,
+    React.createElement(SeedsOfHopeApp)
+  )
 );
